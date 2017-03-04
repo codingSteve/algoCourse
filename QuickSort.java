a31755c4f9a9378ed223f7d31b40a7d8d5489a33
spowell333 <thirtythreeandathird@gmail.com>
2017-03-04 12:43:14 +0000

    complete basic implementation

--- QuickSort.java
+++ QuickSort.java
@@ -4,46 +4,89 @@ public class QuickSort {
 
 	private static final int[] testInts = new int[]{2148, 9058, 7742, 3153, 6324, 609, 7628, 5469, 7017, 504};
 
-	public static void main(final String[] ARGV ) {
+	public static void main(final String[] ARGV ) throws Exception {
 
 		for ( int i = 0 ; i < ARGV.length ; i++ ) { 
 			if ( "--test".equals(ARGV[i]) ) { 
-				int[] s = new int[testInts.length];
-				int[] t = new int[testInts.length];
-				System.arraycopy(testInts, 0, s, 0, testInts.length);
-				System.arraycopy(testInts, 0, t, 0, testInts.length);
-				Utils.logStrings( "Arrays.sort..." );
-				Arrays.sort(s);
-				Utils.logInts( s );
-				Utils.logStrings( "QuickSort.sort..." );
-				sort( t );
-				Utils.logInts( t );
-
-				for ( int j = s.length ; --j >= 0 ; ) { 
-					if  ( s[j] != t[j] ) { 
-						Utils.logStrings(" Failed at position ", ""+ j);
-						break;
-					}
-				}
+        compare(testInts);
 			}
+      else if ( "--file".equals(ARGV[i]) ) {
+        compare(Utils.fileToInts( ARGV[++i]) );
+      }
 		}
 
 
 	}
 
-	public static void sort(final int[] ints ) { 
+  public static void compare( int[] input ) { 
+		int[] s = new int[input.length];
+		int[] t = new int[input.length];
+		System.arraycopy(input, 0, s, 0, input.length);
+		System.arraycopy(input, 0, t, 0, input.length);
+		Utils.logStrings( "Arrays.sort..." );
 
-		final Tuple t = choosePivot(ints);
-		for ( int i = ints.length ; -- i >=0;){
-			if ( ints[i] < t._1 ) { 
+    long start = System.nanoTime();
+		Arrays.sort(s);
+    long end = System.nanoTime();
 
+		Utils.logInts( s );
+		Utils.logStrings( "Arrays.sort duration " + ((int)( end - start ) / 1000),  "\n", "QuickSort.sort..." );
+
+    start = System.nanoTime();
+		sort( t );
+    end = System.nanoTime();
+
+		Utils.logInts( t );
+		Utils.logStrings( "QuickSort.sort duration " + ((int)( end - start ) / 1000));
+
+		for ( int j = s.length ; --j >= 0 ; ) { 
+			if  ( s[j] != t[j] ) { 
+				Utils.logStrings(" Failed at position ", ""+ j);
+				break;
 			}
 		}
+  }
 
+	public static void sort(final int[] ints){ sort(ints, 0, ints.length - 1 ); }
+
+	public static void sort(final int[] ints, int from, int to ) { 
+    if ( from >= to || from < 0 || to >= ints.length ) return;
+
+		final Tuple p = choosePivot(ints, from , to );
+    swap(ints, from, p._2);
+
+    int i = from + 1;
+    //Utils.logStrings("Starting with from = " + from, "to = "+to, "pivot = " + p);
+    //Utils.logInts(ints);
+
+    for (int j = from+1; j <= to; j++ ) {
+      //Utils.logInts(i, j);
+      if ( ints[j] < p._1 ) {
+        swap(ints, j, i);
+        //Utils.logInts( ints );
+        i++;
+      }
+
+    }
+    int pivotTarget = i - 1;
+    swap(ints, from, pivotTarget );
+
+    //System.out.print("partitioned about " + p._1 + " : ");
+    //Utils.logInts(ints);
+
+    sort( ints, from, pivotTarget - 1 );
+    sort( ints, pivotTarget + 1, to);
 
 	}
 
-	public static Tuple choosePivot(final int[] ints) { return new Tuple(ints[0], 0); }
+  public static void swap(final int[] ints, int a, int b) { 
+    if ( a == b ) return;
+    final int c = ints[a]; ints[a] = ints[b]; ints[b] = c;
+  }
+
+	public static Tuple choosePivot(final int[] ints, int from, int to) {
+    return new Tuple(ints[from], from); 
+  }
 
 	public static class Tuple{ 
 		public final int _1;
@@ -53,7 +96,7 @@ public class QuickSort {
 			this._2 = _2;
 		}
 		public String toString(){
-			return "(" + _1 +','+_2+')';
+			return "(val=>" + _1 +", position =>"+_2+')';
 		}
 	}
 
