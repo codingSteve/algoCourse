8e976cd89065ef3797923325324bdff0413d8e6d
spowell333 <thirtythreeandathird@gmail.com>
2017-03-04 12:42:22 +0000

    limit logging loops and add file to ints

--- Utils.java
+++ Utils.java
@@ -1,10 +1,16 @@
+import java.util.*;
+import java.nio.file.*;
+
 public class Utils { 
+  public static int  _loopMax = 20;
 	public static void logInts(int... ints){
 		System.out.print('[');
-		for (int i : ints) {
-			System.out.print(i);
+    int i  =0 ;
+		for ( ; i < ints.length && i < _loopMax ; i++ ) {
+			System.out.print(ints[i]);
 			System.out.print(',');
 		}
+    if ( i == _loopMax ) System.out.print( "... ");
 		System.out.println(']');
 	}
 
@@ -20,12 +26,26 @@ public class Utils {
 	public static void logStrings(String... strings) { 
 		System.out.print('[');
 		
-		for(String s : strings) { 
-			System.out.print(s);
+    int i = 0;
+		for(; i < strings.length && i < _loopMax ; i++ ) { 
+			System.out.print(strings[i]);
 			System.out.print(',');
 		}
+    if ( i == _loopMax ) System.out.print( "... ");
 		System.out.println(']');
 	}
+  public static int[] fileToInts( final String fileName ) throws Exception {
+        final Path p = FileSystems.getDefault().getPath(fileName);
+        final List<String> numbers = Files.readAllLines(p);
+        return stringsToInts(numbers);
+  }
+
+	public static int[] stringsToInts( final List<String> strings ){
+		int[] ints = new int[strings.size()];
+		for( int i = strings.size(); --i >=0;) ints[i] = Integer.valueOf(strings.get(i)).intValue();
+		return ints;
+	}
+
 	public static Integer[] stringsToIntegers( final String[] strings ){
 		Integer[] integers = new Integer[strings.length];
 		for( int i = strings.length; --i >=0;) integers[i] = Integer.valueOf(strings[i]);
