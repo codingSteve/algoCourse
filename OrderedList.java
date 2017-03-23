0381189a287a2fe219a932f5c0f0f6710cd9a17b
Steven Powell <steven.powell@bestcoders.co.uk>
2017-03-23 15:17:18 +0000

    Add remove, isEmpty, addAll etc for the OrderedList

--- OrderedList.java
+++ OrderedList.java
@@ -8,6 +8,8 @@ public abstract class OrderedList<T extends Comparable<T>> implements Iterable<T
   T               _first = null; 
   OrderedList<T>  _rest  = null;
 
+  static boolean _VERBOSE = false;
+
   public Iterator<T> iterator() { 
 
     return new Iterator<T>() { 
@@ -24,14 +26,49 @@ public abstract class OrderedList<T extends Comparable<T>> implements Iterable<T
       }
     };
   }
+  
+  public T remove() { 
+    T head = _first;
+    if ( _VERBOSE ) System.out.println( "before: " + this.toString() ) ;
 
-  public int size() { return 1 + ( ( _rest == null) ? 0 : _rest.size() ) ; } 
+
+    if ( _rest != null ) { 
+      _first = _rest._first;
+      _rest  = _rest._rest; 
+    }
+    else { 
+      _first = null;
+    }
+
+    if ( _VERBOSE ) System.out.println( "after : " + this.toString() );
+    return head;
+  }
+
+  public T remove( T element ) { 
+    if ( element.equals( element ) ) {
+      return this.remove();
+    }
+    else {
+      return ( _rest == null ) ? null :  _rest.remove( element );
+    }
+  }
+
+  public boolean isEmpty() { return _first == null ;}
+
+  public int size() { return (isEmpty()) ? 0 : (1 + ( ( _rest == null) ? 0 : _rest.size() ) ); } 
 
   public String toString() { 
     return ( ( _first == null ) ? "NULL" : _first.toString() ) + ',' + ( (_rest != null) ?  _rest.toString() : "");
   }
 
-  public abstract void add( T e);
+  public void addAll( OrderedList<T> other ) { 
+    for ( T e : other ) { 
+      this.add ( e );
+      if ( _VERBOSE ) System.out.println("adding all: " + this.toString() );
+    }
+  }
+
+  public abstract void add( T e );
 
 
 }
