c3b3fd8af1883036e34fd80e2e9a35adf020e6bf
Steven Powell <codingSteve@users.noreply.github.com>
2017-04-03 19:56:44 +0100

    add stub for TwoSum

---
+++ TwoSum.java
@@ -0,0 +1,10 @@
+/**
+The goal of this problem is to implement a variant of the 2-SUM algorithm covered in this week's lectures.
+
+your task is to find the distinct pairs of integers in the input which sum to an element of the set [-10,000 , 10,000 ]
+
+**/
+
+public class TwoSum {
+
+} 
