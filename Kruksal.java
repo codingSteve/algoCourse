9ccbbfc296f2a21373a5cbe3e75d1836e92b06c8
Steven Powell <steven.powell@bestcoders.co.uk>
2017-04-19 18:58:08 +0100

    Complete working version for Grredy Assignment two Q1

--- Kruksal.java
+++ Kruksal.java
@@ -3,68 +3,96 @@ import java.util.*;
 
 public class Kruksal {
   
-  private static int[][] testCase0      = new int[][]{ {2,1}, {1, 2, 1 }};
-  private static long    expectation0   = 1L; // Pipe cleaning test case.
+  private static int[][] testCase0      = new int[][]{ {3,2}, {1, 2, 1 }, {1, 3, 1}, {2, 3, 10}};
+  private static long    expectation0   = 1L; // Pipe cleaning test case. {1,2} {3} there are two routes across the cut and we expect the minimum one
 
-  private static int[][] testCase1      = new int[][]{ {3,3}, {1, 2, 1 }, {2, 3, 2}, {1,3,1} };
-  private static long    expectation1   = 2L; // MST 3 -> 1 -> 2, cost of 2
+  private static int[][] testCase1      = new int[][]{ {3,2}, {1, 2, 1 }, {1, 3, 20}, {1, 3, 10} };
+  private static long    expectation1   = 10L; // {1, 2}, {3}. there are two routes across the cut and we expect the minimum one
 
-  private static int[][] testCase2      = new int[][] {{ 4, 5 } , { 1, 2, 1 } , { 2, 4, 2 } , { 3, 1, 4 } , { 4, 3, 5 } , { 4, 1, 3 } ,};
-  private static long    expectation2   = 7L; // from https://www.coursera.org/learn/algorithms-greedy/discussions/weeks/1/threads/vZmiuf_9EeaJEAqoFc7ZZA
+  private static int[][] testCase2      = new int[][]{{5, 2}, { 1, 2,  1}, { 1, 3, 100}, { 1, 4, 100}, { 1, 5, 100}, { 2, 3, 100}, { 2, 4, 100}, { 2, 5, 100}, { 3, 4,  10}, { 3, 5,  10}, { 4, 5,  10},};
+  private static long    expectation2   = 100L; // https://www.coursera.org/learn/algorithms-greedy/discussions/weeks/2/threads/iC2LwBGIEeeLRw7GFY8sFA
 
-  private static int[][] testCase3      = new int[][] {{ 6, 10 } , {1, 2, 6,}, {1, 4, 5,}, {1, 5, 4,}, {2, 4, 1,}, {2, 5, 2,}, {2, 3, 5,}, {2, 6, 3,}, {3, 6, 4,}, {4, 5, 2,}, {5, 6, 4,}};
-  private static long    expectation3   = 14L; // This is from Algorithms "DPV" page 139:
+  private static int[][] testCase3      = new int[][]{{4, 2}, {1, 2, 1}, {1, 3, 4}, {1, 4, 3}, {2, 4, 2}, {3, 4, 5}};
+  private static long    expectation3   = 4L;
 
-  private static int[][] testCase4      = new int[][] {{3, 2}, {1,2,1<<30}, {2,3,1<<30}};
-  private static long    expectation4   = (1L<<30) + (1L<<30); // simple pipe cleaner for long paths
-
-  private static int[][][] testCases    = new int[][][] { testCase0   , testCase1    , testCase2    , testCase3    , testCase4    };
-  private static long[]    expectations = new long[]    { expectation0, expectation1 , expectation2 , expectation3 , expectation4 };
+  private static int[][][] testCases    = new int[][][] { testCase0   , testCase1    , testCase2    , testCase3    };
+  private static long[]    expectations = new long[]    { expectation0, expectation1 , expectation2 , expectation3 };
 
   private static boolean _loud = false;
 
-  public static void main( String[] ARGV ) {
+  public static void main( String[] ARGV ) throws Exception {
 
     int times = 1;
 
     for ( int i = 0 ; i < ARGV.length ; i ++ ) { 
       if ("--times".equals(ARGV[i])) times = Integer.valueOf( ARGV[i+1] ).intValue();
       
-      // else if ( "--file".equals( ARGV[i] )) { 
-      //   int[][] rawInput = Utils.fileToRaggedArrayOfInts( ARGV[ i+1 ], " " );
-      //   for ( int k = times ; --k >=0 ; ) {
-      //       Node n = prepareGraph( rawInput );
-      //       long start = System.nanoTime();
-      //       long mstCost = mst( rawInput[0][0], rawInput[0][1], n);
-      //       long duration = System.nanoTime() - start;
+      else if ( "--file".equals( ARGV[i] )) { 
+        int[][] rawInput = Utils.fileToRaggedArrayOfInts( ARGV[ i+1 ], " " );
+        System.out.format("File %s has %d edges%n", ARGV[ i+1], rawInput.length -1 );
 
-      //       System.out.format("File %s, run %4d calculated a cost of %10d in %6dµs%n", 
-      //         ARGV[ i+1 ], k, mstCost, duration /1000);
-      //     }
-      // }
+        for ( int k = times ; --k >=0 ; ) {
+            long preprocessStart = System.nanoTime(); 
+            PriorityQueue<Edge> edges = new PriorityQueue<>(rawInput.length - 1);
+            long preprocessDuration = System.nanoTime() - preprocessStart;
+            System.out.format("File %s preprocessed in %6dµs%n",ARGV[ i+1],preprocessDuration/1000 );
+
+            Map<Integer,Node> nodes = prepareGraph( rawInput, edges );
+
+            long start = System.nanoTime();
+            mst( rawInput[0][0], edges, 4);
+            long duration = System.nanoTime() - start;
+
+            Edge nextEdge = edges.poll();
+            while ( nextEdge._head._leader == nextEdge._tail._leader) 
+              nextEdge = edges.poll();
+
+            long minSeparation = nextEdge._length;
+
+            System.out.format("File %s, run %4d calculated a minSeparation of %10d in %6dµs%n", 
+              ARGV[ i+1 ], k, minSeparation, duration /1000);
+          }
+      }
       else if ("--loud".equals( ARGV[i])) _loud = true;
+      else if ("--quiet".equals( ARGV[i])) _loud = false;
       else if ("--test".equals( ARGV[i] )){
+        
         boolean allPassed = true;
         for ( int j = testCases.length ; --j >= 0 && allPassed ;  ) {
           
           for ( int k = times ; --k >=0 ; ) {
 
-            Map<Node, List<Node>> connectedComponents = new HashMap<>(testCases[j][0][0]);
             PriorityQueue<Edge> edges = new PriorityQueue<>(testCases[j].length - 1);
 
-            prepareGraph( testCases[ j ], edges, connectedComponents );
+            Map<Integer, Node> nodes = prepareGraph( testCases[ j ], edges );
 
             long start = System.nanoTime();
             if ( _loud ) System.out.format( "About to process a graph of %d node(s) and %d edge(s)%n", 
               testCases[j][0][0], testCases[j].length -1 );
 
-            long mstCost = mst( testCases[j][0][0], testCases[j][0][1], edges, connectedComponents);
+            mst( testCases[j][0][0], edges, testCases[j][0][1]);
+
             long duration = System.nanoTime() - start;
 
-            allPassed = (mstCost == expectations[j]);
+            Edge nextEdge = edges.poll();
+            while ( nextEdge._head._leader == nextEdge._tail._leader) 
+              nextEdge = edges.poll();
+
+            long minSeparation = nextEdge._length;
+
+            if ( _loud ) { 
+              for (Map.Entry<Integer, Node> e : nodes.entrySet() ) {
+                if ( e.getValue() == e.getValue()._leader) {
+                  System.out.format("NodeID %d _followers: %s%n", e.getValue()._nodeID, e.getValue()._followers.toString() );
+                }
+              }
+
+            }
+
+            allPassed = (minSeparation == expectations[j]);
 
             System.out.format("test case %2d, run %4d calculated a cost of %10d (expected %10d) in %6dµs, passed %b %n", 
-              j, k, mstCost, expectations[j], duration /1000, allPassed);
+              j, k, minSeparation, expectations[j], duration /1000, allPassed);
           }
         }
       }
@@ -74,7 +102,7 @@ public class Kruksal {
 
   }
 
-   public static void prepareGraph( int[][] input, PriorityQueue<Edge> edges, Map<Node, List<Node>> connectedComponents ) { 
+   public static Map<Integer, Node> prepareGraph( int[][] input, PriorityQueue<Edge> edges ) { 
     int nodeCount = input[0][0];
     Map<Integer, Node> nodes = new HashMap<>( nodeCount );
 
@@ -94,55 +122,67 @@ public class Kruksal {
         tail  = new Node(tailID); nodes.put( tailID, tail);
       }
 
-      if ( _loud ) System.out.format("Created edge %s %n", new Edge(head, tail, input[i][2]) );
+      edges.offer( new Edge(head, tail, input[i][2]) );
 
     }
-
-    for ( Map.Entry<Integer, Node> e : nodes.entrySet() ) return e.getValue();
-    return null;
+    return nodes;
 
   }
 
-  public static long mst( int nodeCount, int edgeCount, PriorityQueue<Edge> edges, Map<Node, List<Node>> connectedComponents ) {
+  public static void mst( int nodeCount, PriorityQueue<Edge> edges, int requiredClusters) {
     long cost = 0L;
-    int requiredNodes = nodeCount;
+    int clusters = nodeCount;
     
-    while ( requiredNodes != 0 ) {
+    while ( requiredClusters != clusters ) {
+      if ( _loud ) System.out.println("clusters == " + clusters + ", edgesRemaining ==  " + edges.size());
       Edge cheapest = edges.poll();
-      if ( cheapest._tail == cheapest._head._leader ) continue;
+      if ( cheapest._tail._leader == cheapest._head._leader ) continue;
 
-      union( cheapest._head, cheapest._tail, connectedComponents );
+      if ( _loud ) logEdge(cheapest);
+
+      union( cheapest._head, cheapest._tail );
       cost += cheapest._length;
-      requiredNodes--;
+      clusters--;
+
     }
 
-    return cost;
   }
 
+private static void logEdge( Edge e ){
+  System.out.format("About to add %s to the mst%n", e.toString());
+}
   
 
-  private static void union( Node x, Node y, Map<Node, List<Node>> connectedComponents ) {
+  private static void union( Node x, Node y ) {
     
-    final List<Node> xFollowers = connectedComponents.get(x);
-    final List<Node> yFollowers = connectedComponents.get(y);
+    final List<Node> xFollowers = x._leader._followers;
+    final List<Node> yFollowers = y._leader._followers;
 
-    List<Node> union      = xFollowers;
-    List<Node> recruits   = yFollowers;
-    Node       newLeader  = x._leader;
+    final List<Node> enlisted;   
+    final List<Node> recruits;   
+    final Node       newLeader;  
 
     if ( yFollowers.size() > xFollowers.size() ) {
-      union = yFollowers;
-      recruits = xFollowers;
+      enlisted  = yFollowers;
+      recruits  = xFollowers;
       newLeader = y._leader;
+      x._leader = y._leader;
+    }
+    else {
+      enlisted   = xFollowers;
+      recruits   = yFollowers;
+      newLeader  = x._leader;
+      y._leader  = x._leader;
     }
 
-    x._leader = newLeader;
-    y._leader = newLeader;
+    Iterator<Node> it = recruits.iterator();
 
+    while ( it.hasNext() ){ 
+      Node n = it.next();
+      it.remove();
 
-    for ( Node n : recruits ){
       n._leader = newLeader;
-      union.add( n );
+      enlisted.add( n );
     }
   }
 
