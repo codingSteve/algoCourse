af60cfebf70a2e37653478dc340c73d17e2bb991
Steven Powell <steven.powell@bestcoders.co.uk>
2017-04-19 11:39:04 +0100

    Add hamming distance routine

--- Utils.java
+++ Utils.java
@@ -3,6 +3,43 @@ import java.nio.file.*;
 
 public class Utils { 
   public static int  _loopMax = 20;
+
+  /** Routine to count the different bits between two integers.
+	*
+    * This is taken from Wikipedia, it's shorter and will execute the loop
+    * fewer times.
+    * 
+    * Hamming distance: {@link https://en.wikipedia.org/wiki/Hamming_distance}
+    */
+  public static int hamDistance( final int x, final int y ) {
+      int z = (x ^ y);
+      int d = 0 ;
+      while ( z != 0 ) {
+          d++;
+          z &= z - 1;
+      }
+      return d;
+  }
+
+  /** Routine to count the different bits between two integers.
+  *
+  * This is a naive implementation which is longer and will execute the main 
+  * loop more times than the version above. 
+  * 
+  * Hamming distance: {@link https://en.wikipedia.org/wiki/Hamming_distance}
+  */
+  @Deprecated
+  private static int hamDistanceS( final int x, final int y ) {
+      int z = (x ^ y);
+      int d = 0 ;
+      while ( z != 0 ) {
+          d += ( z & 1 );
+          z >>= 1;
+      }
+      return d;
+  }
+
+
   public static void logRaggedInts( int[]... twoDimensionalArray ) {
     int i = 0;
 
