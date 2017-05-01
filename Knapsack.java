810e1efc8c4c1a35afd68b5d2cd3bc4d4ca127b3
Steven Powell <steven.powell@bestcoders.co.uk>
2017-05-01 08:38:01 +0100

    Add total value lookup and test case from the lectures

--- Knapsack.java
+++ Knapsack.java
@@ -1,8 +1,14 @@
 public class Knapsack {
   private static boolean _loud = false;
 
-  private static int[][][] testCases    = new int[][][]{};
-  private static int[]     expectations = new int[]{};
+  private static int[][]   testCase0    = new int[][]{{1, 1}, {1, 1}};
+  private static int       expectation0 = 1; // pipe cleaning test case;
+
+  private static int[][]   testCase1    = new int[][]{{6,4}, {3,4}, {2,3},{4,2}, {4,3}};
+  private static int       expectation1 = 8; // from the lectures
+
+  private static int[][][] testCases    = new int[][][]{ testCase0   , testCase1   , };
+  private static int[]     expectations = new int[]{     expectation0, expectation1, };
 
   public static void main(String[] args) throws Exception {
     int times = 1;
@@ -17,7 +23,9 @@ public class Knapsack {
       else if ("--times".equals(args[i])) {
         times = Integer.valueOf( args[ ++ i ] );
       }
-      else if ( "--test".equals(args[i])) { 
+      else if ( "--test".equals(args[i])) {
+        
+        TESTING:
         for (int j = 0 ; j < testCases.length ; j++ ) {
           for (int k = times ; --k >=0 ; ) {
             long start = System.nanoTime();
@@ -28,11 +36,14 @@ public class Knapsack {
               Utils.logRaggedInts( solution );
             }
 
-            int totalValue   = -1; // final element of the grid
-
-            // int[] selectedItems = reconstruct( solution );
+            int totalValue   = solution[testCases[j][0][1]][testCases[j][0][0]]; // final element of the grid
 
             long duration = System.nanoTime() - start;
+
+            System.out.format("Run %2d of testCase %2d produced value of %6d (expected %6d) in %6dµs%n",
+              k, j, totalValue, expectations[j], duration / 1000);
+
+            if (totalValue != expectations[j]) break TESTING;
             
           }
           
@@ -46,7 +57,34 @@ public class Knapsack {
   }
 
   static int[][] fill( int[][] input ){
-    int[][] solution = new int[][]{};
+    final int W = input[0][0];
+    final int N = input[0][1];
+
+    // plus one here to line up with the input array
+    int[][] solution = new int[N+1][W+1];
+
+    for( int i = 1 ; i < input.length ; i++  ){
+      final int vi = input[i][0];
+      final int wi = input[i][1];
+
+
+      for ( int x = wi ; x <= W ; x++ ) {
+
+        int[] previousColumn = solution[ i-1 ];
+        int previousBest     = previousColumn[x];
+        int newValue         = previousColumn[x-wi] + vi;
+
+        if ( _loud ) System.out.format("i=%d, x=%d pb=%d, ns=%d%n",
+          i, x, previousBest, newValue);
+
+        solution[i][x] = Math.max( previousBest, newValue);
+
+      if ( _loud ) Utils.logRaggedInts( solution );
+        
+      }
+
+    }
+
     return solution;
   }
 
